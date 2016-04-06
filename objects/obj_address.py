49dcfc5c222f78746fc0dde7736442653a1221f0
pbryzek <bryzek@gmail.com>
2016-04-05 22:53:42 -0700

    fix hot words

--- objects/obj_address.py
+++ objects/obj_address.py
@@ -2,7 +2,7 @@ import string
 
 class AddressObj(object):
     
-    hot_words = ["cheap", "handy man", "fast"]
+    hot_words = ["reo", "vacant", "foreclosure", "short sale", "fixer upper", "distressed property", "distressed owner", "probate sale", "divorce", "as is", "motivated seller", "handyman special", "investor special", "tlc", "needs work", "potential", "price reduction", "reduced price", "transaction fell through", "build the home of your dreams", "BOM", "Damaged", "Teardown", "Fire damage", "Water damage", "Redevelopment opportunity", "Needs remodeling", "Needs updating", "Needs renovation", "Contractor special", "Land value", "Dilapidated", "Add on", "Build new"]
 
     def __init__(self, address, city, state, zip, redfin_link, dom, listing_id, description, price):
         self.address = address.replace("(Unable to map)", "")
@@ -17,11 +17,11 @@ class AddressObj(object):
         self.listing_id = listing_id
 
         exclude = set(string.punctuation)
-        description = ''.join(ch for ch in description if ch not in exclude)
+        description = ''
 
         clean_description = description.strip().lower()
 
         self.num_hot_words = 0
-        for hot_word in self.hot_words:
+        for hot_word.strip().lower() in self.hot_words:
             if hot_word in clean_description:
                 self.num_hot_words += 1
